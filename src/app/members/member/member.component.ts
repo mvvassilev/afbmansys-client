1a71a8c1b3dbf95fa01d1ab28c359a44bb37e388
mvvassilev <m.v.vassilev@abv.bg>
2020-02-10 19:26:14 +0200

    Member page styling configs

--- src/app/members/member/member.component.ts
+++ src/app/members/member/member.component.ts
@@ -12,6 +12,30 @@ export class MemberComponent implements OnInit {
   member: Member
   // TEMP
   id: string
+  metadataTop = [
+    ['Чл. номер:', '0001'],
+    ['ЕГН:', '9708137241'],
+    ['Име:', 'Момчил Василев'],
+    ['Телефон:', '0886744800'],
+    ['Email:', 'm.v.vassilev@abv.bg'],
+    ['Специалност:', 'Физиотерапевт'],
+    ['Адрес:', 'София, кв.Филиповци, ул.Люлин 22'],
+  ]
+
+  metadataBottom = [
+    ['Пол:', 'Мъж'],
+    ['Дата:', '01/01/2020'],
+    ['Регион:', 'Западен'],
+    ['Служебен адрес:', 'София, кв.Филиповци, ул.Люлин 22'],
+    ['Адрес за кореспонденция:', 'София, кв.Филиповци, ул.Люлин 22']
+  ]
+  // TEMP
+  /**
+        public workContractID: string,
+        public mainAddress: string,
+        public declaration: boolean,
+        public penaltyID: string
+   */
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
